644d9f12e11dc3bfcd871065504059712a9cbf6a
SDH <sdh@Mac-mini-de-SDH.sdh.pri>
2023-03-01 10:48:24 +0100

    End of section 9 with add of gallery with ngx gallery

--- src/app/members/members-detail/members-detail.component.ts
+++ src/app/members/members-detail/members-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { Member } from 'src/app/_model/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -11,16 +12,40 @@ import { MembersService } from 'src/app/_services/members.service';
 export class MembersDetailComponent implements OnInit {
 
   member :Member | undefined;
+  galleryOptions: NgxGalleryOptions[] =[];
+  galleryImages: NgxGalleryImage[] = [];
 
   constructor(private memberService : MembersService, private route :ActivatedRoute ) { }
 
   ngOnInit(): void {
     this.loadMember();
-    console.log(this.member);
+
+    this.galleryOptions = [
+      {
+      width: '500px',
+      height: '500px',
+      imagePercent: 100,
+      thumbnailsColumns:4,
+      imageAnimation: NgxGalleryAnimation.Slide,
+      preview: false
+      }
+  ]
 
 
     }
 
+    getImages(){
+      if(!this.member)return [];
+      const imageUrls =[];
+      for(const photo of this.member.photos ){
+        imageUrls.push({
+          small: photo.url,
+          medium: photo.url,
+          big: photo.url
+        })
+      }
+      return imageUrls;
+    }
   loadMember(){
     const username = this.route.snapshot.paramMap.get('username');
     console.log(username);
@@ -28,9 +53,8 @@ export class MembersDetailComponent implements OnInit {
     if(!username) return;
     this.memberService.getMember(username).subscribe({
       next:member =>{
-        console.log(member.city);
-
-        this.member = member
+        this.member = member;
+        this.galleryImages = this.getImages();
 
       } ,
 
